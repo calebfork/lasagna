e132040e911d343625d723511ccf3d9290f580e7
Caleb F <61980609+calebfork@users.noreply.github.com>
2020-03-09 10:14:12 -0500

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,11 @@
+var navbar = '<div class="container circleBehind">\
+				<a href="homepage.html">LOGOUT</a>\
+				<a href="about.html">ABOUT</a>\
+				<a href="portfolio.html">PORTFOLIO</a>\
+				<a href="resume.html">RESUME</a>\
+				<a href="contact.html">CONTACT</a>\
+			  </div>';
+
+function loadNav(){
+	document.getElementById('nav').innerHTML = navbar;
+}
